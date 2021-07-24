fc6abb1049f880ec4441fa2ea94f2af18d7d3985
blaise.hackler <blaise.hackler@gmail.com>
2021-07-24 12:52:42 -0500

    configure connection strings; add both local and prod db connections; create db initializer for setting up dummy data

--- Kahuna/Kahuna.MVC/Startup.cs
+++ Kahuna/Kahuna.MVC/Startup.cs
@@ -32,15 +32,16 @@ namespace Kahuna.MVC
             //Use SQL Database if in Azure, otherwise, use SQLite
             if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
             {
-                services.AddDbContext<MyDatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+                //services.AddDbContext<MyDatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AzureConnection")));
             }
             else
             {
-                services.AddDbContext<MyDatabaseContext>(options => options.UseSqlite("Data Source=MvcMovie.db"));
+                //services.AddDbContext<MyDatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LocalConnection")));
             }
 
-            //Automatically perform database migration
-            services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();
+            ////If you make updates to your models, this will update the database
+            ////Automatically perform database migration
+            //services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
